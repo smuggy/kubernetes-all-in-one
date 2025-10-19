9dcc3f620953b4679936ae2694bf4f9031b949a6
marc <marc.mckernan@gmail.com>
2025-10-19 09:44:01 -0500

    update to setup node for use

--- resources/k3s.tf
+++ resources/k3s.tf
@@ -19,6 +19,24 @@ resource "ssh_resource" "retrieve_config" {
   private_key = tls_private_key.ssh_key.private_key_pem # var.ssh_private_key_pem
 }
 
+resource "ssh_resource" "config_k9s" {
+  depends_on = [
+    ssh_resource.retrieve_config
+  ]
+  host = local.public_ip
+  commands = [
+    "sudo mkdir --parents /root/.kube && sudo cp /etc/rancher/k3s/k3s.yaml /root/.kube/config",
+    "wget https://github.com/derailed/k9s/releases/download/v0.50.15/k9s_Linux_amd64.tar.gz",
+    "tar --extract --file k9s_Linux_amd64.tar.gz",
+    "sudo mv k9s /usr/local/bin/k9s",
+    "mkdir --parents /home/ubuntu/.kube && sudo cp /etc/rancher/k3s/k3s.yaml /home/ubuntu/.kube/config",
+    "sudo chown ubuntu:ubuntu /home/ubuntu/.kube/config",
+    "rm --force k9s_Linux_amd64.tar.gz LICENSE README.md"
+  ]
+  user        = local.user_name
+  private_key = tls_private_key.ssh_key.private_key_pem # var.ssh_private_key_pem
+}
+
 resource "local_file" "kube_config_server_yaml" {
   filename = format("%s/../secrets/%s", path.root, "kube_config_server.yaml")
   content  = ssh_resource.retrieve_config.result
--- resources/outputs.tf
+++ resources/outputs.tf
@@ -1,3 +1,7 @@
 output "public_ip" {
   value = aws_instance.node.public_ip
 }
+
+output "k9s_install" {
+  value = ssh_resource.config_k9s.result
+}
